6fc3ad509713e03e4c1fdb3656a9980d2c0fa9d5
df@ <@>
2022-05-15 12:28:22 +0100

    downloading hmtl works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+const URL_TEMPLATE: &str = "https://videa.hu/kategoriak/film-animacio?sort=0&category=0&page=";
+
+fn main() -> Result<(), reqwest::Error> {
+    let page_count = 1;
+    for index in 1..page_count + 1 {
+        let url = format!("{}{}", URL_TEMPLATE, index);
+        let response = reqwest::blocking::get(url)?;
+        let text = response.text()?;
+        println!("{}", text);
+    }
+    Ok(())
 }
