1c1642796828d6116c792bd0323ba19679be7b86
df@ <@>
2022-05-15 15:04:47 +0100

    refactored; movie is a struct now; wrote better display.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
+const PAGE_COUNT: u16 = 1;
 const URL_TEMPLATE: &str = "https://videa.hu/kategoriak/film-animacio?sort=0&category=0&page=";
+const TITLE_REGEX_PATTERN: &str = r#"<div class="panel-video-title"><a href="(.*)" title=".*">(.*)</a></div>"#;
+
+struct Movie {
+    title: String,
+    url: String,
+}
+
+impl Movie {
+    fn from_capture(cap: regex::Captures) -> Self {
+        Movie {
+            title: cap[2].to_owned(),
+            url: cap[1].to_owned(),
+        }
+    }
+}
+
+impl std::fmt::Display for Movie {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:<70}{}", self.title, self.url)
+    }
+}
 
 fn main() -> Result<(), reqwest::Error> {
-    let page_count = 1;
-    let title_regex_pattern = r#"<div class="panel-video-title"><a href="(.*)" title=".*">(.*)</a></div>"#;
-    // let title_regex_pattern = r#"panel-video-title"#;
-    let re = regex::Regex::new(title_regex_pattern).unwrap();
-    for index in 1..page_count + 1 {
+    let re = regex::Regex::new(TITLE_REGEX_PATTERN).unwrap();
+    let mut movies: Vec<Movie> = vec![];
+
+    for index in 1..PAGE_COUNT + 1 {
         let url = format!("{}{}", URL_TEMPLATE, index);
         let response = reqwest::blocking::get(url)?;
         let text = response.text()?;
+
         for cap in re.captures_iter(&text) {
-            let movie_url = &cap[1];
-            let movie_title = &cap[2];
-            println!("{:<70}{}", movie_title, movie_url);
+            movies.push(Movie::from_capture(cap));
         }
     }
+
+    for movie in movies {
+        println!("{}", movie);
+    }
+
     Ok(())
 }
