7872260c2ebce3e0642cb45868ea53d7af458cef
df@ <@>
2022-05-15 14:24:25 +0100

    getting a single page works.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ const URL_TEMPLATE: &str = "https://videa.hu/kategoriak/film-animacio?sort=0&cat
 
 fn main() -> Result<(), reqwest::Error> {
     let page_count = 1;
+    let title_regex_pattern = r#"<div class="panel-video-title"><a href="(.*)" title=".*">(.*)</a></div>"#;
+    // let title_regex_pattern = r#"panel-video-title"#;
+    let re = regex::Regex::new(title_regex_pattern).unwrap();
     for index in 1..page_count + 1 {
         let url = format!("{}{}", URL_TEMPLATE, index);
         let response = reqwest::blocking::get(url)?;
         let text = response.text()?;
-        println!("{}", text);
+        for cap in re.captures_iter(&text) {
+            let movie_url = &cap[1];
+            let movie_title = &cap[2];
+            println!("{:<70}{}", movie_title, movie_url);
+        }
     }
     Ok(())
 }
