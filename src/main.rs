5b57473871405eb2eb85e7901e4acf1f7149cb23
df@ <@>
2022-05-15 16:05:04 +0100

    blacklist is now coming from the file "blacklist.txt"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 const MAX_PAGES: u16 = 250;
 const URL_TEMPLATE: &str = "https://videa.hu/kategoriak/film-animacio?sort=0&category=0&page=";
 const TITLE_REGEX_PATTERN: &str = r#"<div class="panel-video-title"><a href="(.*)" title=".*">(.*)</a></div>"#;
@@ -23,18 +25,19 @@ impl std::fmt::Display for Movie {
 }
 
 fn main() -> Result<(), reqwest::Error> {
+    let mut file = std::fs::File::open("blacklist.txt").unwrap();
+    let mut blacklist = String::new();
+    file.read_to_string(&mut blacklist).unwrap();
     let matches = clap::Command::new("vidatitles")
         .arg(clap::Arg::new("pagecount").default_value("1"))
         .get_matches();
     let page_count = matches.value_of("pagecount").unwrap().parse::<u16>().unwrap();
+
     if page_count < 1 || page_count > MAX_PAGES {
         println!("Page count must be in range: 1 - 100.");
         return Ok(());
     }
-    let black_list = vec![
-        "Λεπτά",
-        "може",
-    ];
+
     let re = regex::Regex::new(TITLE_REGEX_PATTERN).unwrap();
     let mut movies: Vec<Movie> = vec![];
 
@@ -45,7 +48,7 @@ fn main() -> Result<(), reqwest::Error> {
 
         'cap: for cap in re.captures_iter(&text) {
             let movie = Movie::from_capture(cap);
-            for phrase in &black_list {
+            for phrase in blacklist.lines() {
                 if movie.title.contains(phrase) {
                     continue 'cap;
                 }
